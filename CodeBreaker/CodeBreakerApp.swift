cf69b19f80e052512897b8e433aec72f73e87fd3
K-Elb <kareemkimo711@hotmail.com>
2025-12-26 14:52:50 +0400

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Karim Elbehiri on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CodeBreakerView()
+        }
+    }
+}
